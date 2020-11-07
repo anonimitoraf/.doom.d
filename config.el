0c389c15b9426a7a748091380722db3aefea8a33
Rafael Nicdao <nicdaoraf@gmail.com>
2020-11-08 02:02:13 +1100

    Auto-start gherkin-mode for .feature files

--- config.el
+++ config.el
@@ -315,7 +315,10 @@
 
 (setq typescript-indent-level 2)
 (add-hook 'js2-mode-hook (lambda () (setq js2-basic-offset 2)))
+
 (require 'gherkin-mode)
+(add-to-list 'auto-mode-alist '("\\.feature\\'" . gherkin-mode))
+
 
 ;; --- E-shell stuff ---------------------------------------------------
 ;; Company mode in eshell makes it lag
