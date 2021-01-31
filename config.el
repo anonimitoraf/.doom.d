bd13422fe6fae3106fd111858b275f8bd7a3c7f0
Rafael Nicdao <nicdaoraf@gmail.com>
2021-01-31 18:38:49 +1100

    Remove side line-numbers (since they're already displayed in the modeline)

--- config.el
+++ config.el
@@ -623,6 +623,9 @@
 (after! counsel
   (setq counsel-rg-base-command "rg -M 240 --with-filename --no-heading --line-number --color never %s || true"))
 
+;; Line numbers are already displayed in the modeline
+(setq display-line-numbers-type nil)
+
 ;; --- Godot ---
 
 (require 'gdscript-mode)
