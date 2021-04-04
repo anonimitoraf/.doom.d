1dee0a761a45bb4a392b862b1a984de700628e37
Rafael Nicdao <nicdaoraf@gmail.com>
2021-04-04 20:51:31 +1000

    Workaround for displaying lsp-ui-peek in a childframe

--- config.el
+++ config.el
@@ -565,6 +565,33 @@
                        "[/\\\\]resources$"))
     (add-to-list 'lsp-file-watch-ignored to-ignore)))
 
+;; Copied from https://github.com/emacs-lsp/lsp-ui/issues/441
+;; --- Display lsp-ui-peek in a childframe so that the whole screen is used despite multiple windows
+(defun lsp-ui-peek--peek-display (src1 src2)
+  (-let* ((win-width (frame-width))
+          (lsp-ui-peek-list-width (/ (frame-width) 2))
+          (string (-some--> (-zip-fill "" src1 src2)
+                    (--map (lsp-ui-peek--adjust win-width it) it)
+                    (-map-indexed 'lsp-ui-peek--make-line it)
+                    (-concat it (lsp-ui-peek--make-footer))))
+          )
+    (setq lsp-ui-peek--buffer (get-buffer-create " *lsp-peek--buffer*"))
+    (posframe-show lsp-ui-peek--buffer
+                   :string (mapconcat 'identity string "")
+                   :min-width (frame-width)
+                   :poshandler #'posframe-poshandler-frame-center)))
+
+(defun lsp-ui-peek--peek-destroy ()
+  (when (bufferp lsp-ui-peek--buffer)
+    (posframe-delete lsp-ui-peek--buffer))
+  (setq lsp-ui-peek--buffer nil
+        lsp-ui-peek--last-xref nil)
+  (set-window-start (get-buffer-window) lsp-ui-peek--win-start))
+
+(advice-add #'lsp-ui-peek--peek-new :override #'lsp-ui-peek--peek-display)
+(advice-add #'lsp-ui-peek--peek-hide :override #'lsp-ui-peek--peek-destroy)
+;; ---
+
 ;; Automatically refresh LSP imenu when changing windows
 ;; (add-hook 'window-state-change-hook (cmd! (when (bound-and-true-p lsp-ui-mode)
 ;;                                             (let ((curr-window (selected-window)))
