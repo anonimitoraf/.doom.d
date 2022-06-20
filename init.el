80ffdae55ad2921f1c12e7bc2e0596a1c91ad91e
Rafael Nicdao <nicdaoraf@gmail.com>
2022-06-20 13:35:43 +1000

    Enable lua +lsp

--- init.el
+++ init.el
@@ -154,7 +154,7 @@
        ;;lean
        ;;factor
        ;;ledger            ; an accounting system in Emacs
-       ;;lua               ; one-based indices? one-based indices
+       (lua +lsp)               ; one-based indices? one-based indices
        (markdown +grip)          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
