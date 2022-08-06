ce8f0482b2cafcfd6d03c8382fdd3faccadf6f06
Rafael Nicdao <nicdaoraf@gmail.com>
2022-08-06 22:35:25 +1000

    Add a cmd to evil jumps

--- config.el
+++ config.el
@@ -312,6 +312,7 @@ output as a string."
 (evil-add-command-properties #'find-file :jump t)
 (evil-add-command-properties #'consult-recent-file :jump t)
 (evil-add-command-properties #'doom/find-file-in-private-config :jump t)
+(evil-add-command-properties #'+default/search-buffer :jump t)
 
 (setq +evil-want-o/O-to-continue-comments nil)
 
