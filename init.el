76618d0a6bfb2d812fb9a45a3c80aa38b5cefa24
Rafael Nicdao <nicdaoraf@gmail.com>
2022-09-22 13:42:42 +1000

    Actually, no spell-checking in code comments

--- init.el
+++ init.el
@@ -86,8 +86,7 @@
 
        :checkers
        (syntax +childframe) ; tasing you for every semicolon you forget
-       (spell +aspell
-              +everywhere)             ; tasing you for misspelling mispelling
+       (spell +aspell)             ; tasing you for misspelling mispelling
        grammar           ; tasing grammar mistake every you make
 
        :tools
