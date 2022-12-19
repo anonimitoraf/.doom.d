5120f98c7eaf121fef0aa7ced7c77f7dd3f4a575
Rafael Nicdao <nicdaoraf@gmail.com>
2022-12-19 12:12:04 +1100

    Enable swift

--- init.el
+++ init.el
@@ -183,7 +183,7 @@
        (sh +lsp)                ; she sells {ba,z,fi}sh shells on the C xor
        ;;sml
        ;;solidity          ; do you need a blockchain? No.
-       ;;swift             ; who asked for emoji variables?
+       (swift +lsp)             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
        (web +lsp)               ; the tubes
        (yaml +lsp)
