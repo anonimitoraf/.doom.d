dc351438d051aa3428f6b88cce271153ec8d1a59
Rafael Nicdao <nicdaoraf@gmail.com>
2023-02-26 22:20:31 +1100

    Remove dash docsets (just causes a startup error)

--- config.el
+++ config.el
@@ -1204,8 +1204,6 @@ otherwise, nil."
   (add-to-list 'apheleia-mode-alist '(clojurescript-mode . zprint))
   (add-to-list 'apheleia-mode-alist '(clojurec-mode . zprint)))
 
-(add-to-list 'dash-docs-docsets "Clojure")
-
 (put-clojure-indent 'defjob :defn)
 (put 'defjob 'clojure-doc-string-elt 2)
 (font-lock-add-keywords 'clojure-mode
