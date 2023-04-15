096f02cfd278455d6c873907f814d5209e20056c
Rafael Nicdao <nicdaoraf@gmail.com>
2023-04-15 15:58:52 +1000

    Add cider/clojure niceties

--- sandbox.el
+++ sandbox.el
@@ -1,1 +1,23 @@
 (message "Loading sandbox")
+
+(setq
+  clojure-toplevel-inside-comment-form t
+  cider-show-error-buffer nil
+  cider-auto-jump-to-error nil
+  cider-inspector-max-coll-size 32)
+
+(defvar ++ov-cider-inspector-keywords)
+(defvar ++ov-cider-inspector-method-names)
+(defvar ++ov-cider-inspector-method-param-lists)
+(defun ++ov-cider-inspector ()
+  (ov-clear)
+  (let ((case-fold-search nil))
+    (setq ++ov-cider-inspector-keywords (ov-regexp "public\\|private\\|protected\\|final\\|native\\|static")
+     ++ov-cider-inspector-method-names (ov-regexp "\\([a-zA-Z0-9.$]\\)+(")
+      ++ov-cider-inspector-method-param-lists (ov-regexp "(\\([][<>a-zA-Z0-9.$,]*\\))?")))
+  (ov-set ++ov-cider-inspector-keywords 'face `(:foreground ,(doom-color 'magenta)))
+  (ov-set ++ov-cider-inspector-method-names 'face `(:foreground ,(doom-color 'red)))
+  (ov-set ++ov-cider-inspector-method-param-lists 'face `(:foreground "white")))
+(defun ++ov-cider-inspector-with-delay ()
+  (run-at-time 0 nil #'++ov-cider-inspector))
+(add-hook 'cider-inspector-mode-hook #'++ov-cider-inspector-with-delay)
