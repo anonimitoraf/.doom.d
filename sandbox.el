3488da16f4bd6275789598e8bbc15c52f00ea019
Rafael Nicdao <nicdaoraf@gmail.com>
2023-04-21 08:08:11 +1000

    Add keybindings for git timemachine

--- sandbox.el
+++ sandbox.el
@@ -21,3 +21,8 @@
 (defun ++ov-cider-inspector-with-delay ()
   (run-at-time 0 nil #'++ov-cider-inspector))
 (add-hook 'cider-inspector-mode-hook #'++ov-cider-inspector-with-delay)
+
+(map! :map doom-leader-map
+  "g t" #'git-timemachine
+  "g T" #'git-timemachine-switch-branch)
+
