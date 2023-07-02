81911ca12eb1e2051f48ef2276dbac6c6b77fd46
Rafael Nicdao <nicdaoraf@gmail.com>
2023-07-03 08:05:57 +1000

    Add Mac workaround for not finding libgccjit libs

--- init.el
+++ init.el
@@ -215,3 +215,12 @@
         (with-temp-file tmp
           (insert (with-current-buffer "*Org ORG Export*" (buffer-string))))
         (org-babel-tangle-file tmp)))))
+
+;; Temporary hack to get Emacs to know where libgccjit-related libs are
+(if IS-MAC
+  (setenv "LIBRARY_PATH"
+	  (string-join
+	    '("/opt/homebrew/opt/gcc/lib/gcc/13"
+	       "/opt/homebrew/opt/libgccjit/lib/gcc/13"
+	       "/opt/homebrew/opt/gcc/lib/gcc/13/gcc/aarch64-apple-darwin22/13")
+	    ":")))
