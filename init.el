12b6a146bbdcdd7cb1e0a6b6ad2cf338d99cf509
Rafael Nicdao <nicdaoraf@gmail.com>
2023-10-03 08:22:07 +1100

    Bring back dired ranger

--- init.el
+++ init.el
@@ -75,7 +75,7 @@
        ;; word-wrap         ; soft wrapping with language-aware indent
 
        :emacs
-       (dired +dirvish
+       (dired +ranger
               +icons)             ; making dired pretty [functional]
        ;;electric          ; smarter, keyword-based electric-indent
        ;;ibuffer         ; interactive buffer management
